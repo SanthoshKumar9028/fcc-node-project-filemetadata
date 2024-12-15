182f6d91f0c18a8305518d7ad6196a1dc7fae031
Santhosh <71515865+SanthoshKumar9028@users.noreply.github.com>
2024-12-15 19:10:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require("dotenv").config();
+const URL = require("url").URL;
+let express = require("express");
+const cors = require("cors");
+let path = require("path");
+let bodyParser = require("body-parser");
+const multerUploads = require("multer")({ dest: "uploads/" });
 
-var app = express();
+let app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use((req, res, next) => {
+  console.log(`\n${req.method}-------`);
+  next();
 });
 
+app.use(cors());
 
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(multerUploads.single("upfile"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", (req, res) => {
+  const resObj = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+
+  res.json(resObj);
+});
 
 
 const port = process.env.PORT || 3000;
